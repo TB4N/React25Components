cf339514ff1093d69effe0eca89215d95a96733f
TB4N <131019322+TB4N@users.noreply.github.com>
2024-06-15 02:04:23 +0800

    Update index.jsx

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -6,16 +6,19 @@ export default function RandomColor() {
 const [typeOfColor, setTypeOfColor] = useState('hex');
 const [color, setColor] = useState('#000000')
 
+function handleCreateRandomColor () {
+    
+}
 
     return (
     <div style={{
         width : '100vw',
         height : '100vh',
-        background: color
+        background: color,
         }} className="container">
-        <button>Create HEX Color</button>
-        <button>Create RGB Color</button>
-        <button>Generate Random Color</button>
+        <button onClick={() => setTypeOfColor('hex')}>Create HEX Color</button>
+        <button onClick={() => setTypeOfColor('hex')}>Create RGB Color</button>
+        <button onClick={handleCreateRandomColor}>Generate Random Color</button>
     </div>
     );
 }

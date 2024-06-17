a2047afeddde68953ed56d0c25bcb82525f1aa40
TB4N <131019322+TB4N@users.noreply.github.com>
2024-06-18 07:35:49 +0800

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Accordian from './components/accordian'
 import RandomColor from './components/random-color'
+import StarRating from './components/star-rating'
+import ImageSlider from './components/image-slider'
 
 const App = () => {
   return (
@@ -10,6 +12,13 @@ const App = () => {
 
       {/* Random Coolor Component */}
       <RandomColor/>
+
+      {/* Star Rating Component */}
+      <StarRating/>
+
+      {/* Image Slider Component */}
+      <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>
+
     </div>
   )
 }
